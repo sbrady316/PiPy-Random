921b87e2ee4d7bf47917a4bed584493023f96189
sbradydev <68930842+sbradydev@users.noreply.github.com>
2021-03-26 01:36:38 -0700

    Add new fade routine

--- FirstSense.py
+++ FirstSense.py
@@ -7,17 +7,43 @@ def FadeIn( sense, maxComponent ):
         sense.clear(color)
         time.sleep(0.1)
 
+def Fade(sense, fadeRange):
+    b = (1, 1, 1)
+    g = (0, 64, 0)
 
-purple = (64,0,128)
-halfPurple = (32,0,64)
+    canvas = [
+        b, b, b, b, b, b, b, b,
+        b, b, b, b, b, b, b, b,
+        b, b, b, b, b, b, b, b,
+        b, b, b, b, b, b, b, b,
+        b, b, b, b, b, b, b, b,
+        b, b, b, b, b, b, b, b,
+        b, b, b, b, b, b, b, b,
+        b, b, b, b, b, b, b, b
+    ]
+
+    for i in fadeRange:
+        canvas[i] = halfPurple #g
+        sense.set_pixels(canvas)
+        time.sleep(0.05)
+
+
+
+halfPurple = (64,0,64)
 halfYellow = (64,64,0)
-
-
 
 sense = SenseHat()
 sense.set_rotation(180) # Allows power supply to be away from viewer
-FadeIn (sense, 64)
-
-sense.show_message("Hello, World", text_colour=halfYellow, back_colour=halfPurple)
 sense.clear()
 
+Fade(sense, range(1,64))
+sense.show_message("Hello, World", text_colour=halfYellow, back_colour=halfPurple)
+Fade(sense, range(63,0,-1))
+
+
+# FadeIn (sense, 64)
+
+# #sense.show_message("Hello, World", text_colour=halfYellow, back_colour=halfPurple)
+# sense.clear()
+
+# FadeIn2(sense)
